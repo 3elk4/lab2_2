99501127290b5f1aa11c34144c3ed19778bd8c52
belka <izaa1109@gmail.com>
2020-03-24 19:18:26 +0100

    prototypes of behavior tests for SimilarityFinder

---
+++ src/test/java/SimilarityFinderBehaviorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderBehaviorTest {
+    @Test
+    void bothSequencesAreNull(){
+
+    }
+
+    @Test
+    void oneSequenceIsNull(){
+
+    }
+
+    @Test
+    void sequenceSearcherIsNull(){
+
+    }
+
+    @Test
+    void checkIfSimilarityFinderOperateSequenceSearcher(){
+
+    }
+}
