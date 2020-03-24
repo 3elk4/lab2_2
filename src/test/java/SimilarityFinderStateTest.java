bcf9c43468a8b81c3eabfb1822a091eb24c352cf
belka <izaa1109@gmail.com>
2020-03-24 18:31:26 +0100

    prototypes of state tests for SimilarityFinder

---
+++ src/test/java/SimilarityFinderStateTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+public class SimilarityFinderStateTest {
+
+    @Test
+    void shouldBeBothSequencesEmpty(){
+
+    }
+
+    @Test
+    void shouldBeOneSequenceEmpty(){
+
+    }
+
+    @Test
+    void shouldBeOneSequenceEmpty2(){
+
+    }
+
+    @Test
+    void shouldBeOneCommonValueInSequences(){
+
+    }
+
+    @Test
+    void shouldBeAllCommonValuesInSequences(){
+
+    }
+
+    @Test
+    void shouldBeNoneCommonValuesInSequences(){
+
+    }
+
+    @Test
+    void shouldBeAllCommonValuesExceptOneInSequences(){
+
+    }
+
+
+}
