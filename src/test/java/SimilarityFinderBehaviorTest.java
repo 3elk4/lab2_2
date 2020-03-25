07a24a490d1368edd6c9d063c91d6648be8d116e
belka <izaa1109@gmail.com>
2020-03-25 17:59:11 +0100

    implemented checkIfBothSequencesAreNull

--- src/test/java/SimilarityFinderBehaviorTest.java
+++ src/test/java/SimilarityFinderBehaviorTest.java
@@ -1,9 +1,26 @@
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.search.SimpleSequenceSearcher;
+import edu.iis.mto.similarity.SimilarityFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimilarityFinderBehaviorTest {
+    private SimilarityFinder similarityFinder;
+    private SequenceSearcher searcher;
+    private int[] sequence1 = new int[]{1, 2, 3};
+    private int[] sequence2 = new int[]{4, 5, 6};
+
+    @BeforeEach
+    void init(){
+        searcher = new SimpleSequenceSearcher();
+        similarityFinder = new SimilarityFinder(searcher);
+    }
+
     @Test
     void checkIfBothSequencesAreNull(){
-
+        Assertions.assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, null));
     }
 
     @Test
